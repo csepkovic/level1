d8d65364761cc27d0479a2bb754fc861294ac8bb
najclark <league@WTS7.attlocal.net>
2017-02-16 18:28:44 -0800

    Week 7

---
+++ src/level1/SimpleCalculator.java
@@ -0,0 +1,96 @@
+package level1;
+
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class SimpleCalculator implements MouseListener {
+		JFrame calculator = new JFrame ();
+		JPanel panel = new JPanel ();
+		JButton add = new JButton();
+		JButton multi = new JButton();
+		JButton sub = new JButton();
+		JButton div = new JButton();
+		JTextField number1 = new JTextField();
+		JTextField number2 = new JTextField();
+		JLabel product = new JLabel();
+		
+	public void createUI() {
+		calculator.add(panel);
+		panel.add(number1);
+		panel.add(number2);
+		panel.add(add);
+		panel.add(multi);
+		panel.add(sub);
+		panel.add(div);
+		panel.add(product);
+		calculator.pack();
+		add.setText("Add");
+		multi.setText("Multiply");
+		sub.setText("Subtract");
+		div.setText("Divide");
+		add.addMouseListener(this);
+		multi.addMouseListener(this);
+		sub.addMouseListener(this);
+		div.addMouseListener(this);
+		calculator.setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		SimpleCalculator calc = new SimpleCalculator();
+		calc.createUI();
+		
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		int num1 = Integer.parseInt(number1.getText());
+		int num2 = Integer.parseInt(number2.getText());
+		String finish;
+		if (e.getSource()==add) {
+			finish = Integer.toString(num1 + num2);
+			product.setText(finish);
+		} else if (e.getSource()==multi) {
+			finish = Integer.toString(num1 * num2);
+			product.setText(finish);
+		} else if (e.getSource()==sub) {
+			finish = Integer.toString(num1 - num2);
+			product.setText(finish);
+		} else if (e.getSource()==div) {
+			finish = Integer.toString(num1 / num2);
+			product.setText(finish);
+		}
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+}
